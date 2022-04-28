e425118fbfccc6721a6c9e204f70d5f02da95f42
UnderCruzer <104268228+UnderCruzer@users.noreply.github.com>
2022-04-28 21:02:13 +0900

    Update 10950.cpp

--- Backjoon/10950.cpp
+++ Backjoon/10950.cpp
@@ -1,11 +1,21 @@
 #include <stdio.h>
-int main() {
-	int a, b, t;
-	scanf("%d", &t);
 
-	for (int i = 0; i < t; i++) {
-		scanf("%d %d", &a, &b);
-		printf("%d/n", a + b);
-	}
-	return 0;
+int main()
+
+{
+
+    int n,m,t,i;
+
+    scanf("%d", &t);
+
+    for(i=0; i<t; i++) {
+
+        scanf("%d %d", &n, &m);
+
+        printf("%d\n", n+m);
+
+    }
+
+    return 0;
+
 }
